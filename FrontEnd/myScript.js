e42da93ce33b554bf7008bec15ccbefe0e2a0437
Vedant Sharma <vedant.s27@gmail.com>
2022-06-02 13:29:53 +0530

    Front End updated

--- FrontEnd/myScript.js
+++ FrontEnd/myScript.js
@@ -1,34 +1,50 @@
+// Form Submit Button DOM
+let submitBtn = document.querySelector("#submit");
 
-const getTickets = async() => {
-    // let response = await fetch('http://localhost:8080/booking/1');
-    let response2 = await fetch('http://localhost:8080/booking', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify (
-            {
-                ticket: {
-                ticketId: 6,
-                name: "ABC",
-                seatNumber: 15,
-                price:3000
-                },
-                payment:  {
-                paymentId: 789,
-                status:"Success"
-                                }
-                            }
-                        )
-                    });
-                    // console.log(response);
-                    console.log(response2);
-                }
+// Adding event listener to form submit button
+submitBtn.addEventListener('click', (event) => {
 
-                // if (submit) {
-                //     submit.addEventListener('click', getTickets);
-                // }
+   // Preventing form to submit
+   event.preventDefault(); //stops page from reloading
 
-                // console.log("hello");
-                // alert("hello ppl");
+   // Fetching Form data
+   let ticketid = document.getElementById('ticketid').value;
+   let name = document.getElementById('name').value;
+   let seatnumber = document.getElementById('seatnumber').value;
+   let price = document.getElementById('price').value;
+   let paymentid = document.getElementById('paymentid').value;
+   let status = document.getElementById('status').value;
+   const url = 'http://localhost:8080/booking'
+   const callback = (response) => {
+        console.log(response);
+   }
+   const data = {
+       "ticket":{
+           "ticketId": ticketid,
+           "name": name,
+           "seatNumber": seatnumber,
+           "price":price
+       },
+       "payment":{
+           "paymentId": paymentid,
+           "status":status
+       }
+   }
+
+   // jQuery Ajax Post Request
+   $.ajax({
+       headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           'Access-Control-Allow-Origin': '*',
+           "Access-Control-Allow-Credentials" : true
+       },
+       xhrFields: {withCredentials: true},
+       'type': 'POST',
+       'url': url,
+       'CrossOrigin': true,
+       'data': JSON.stringify(data),
+       'dataType': 'json',
+       'success': callback
+   });
+});
